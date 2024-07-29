369c5cc987be9ef58d6e257b6665da91a03fc05e
hakkam0397 <Hsingh0397@conestogac.on.ca>
2024-07-29 03:13:36 -0400

    updates

--- setting.js
+++ setting.js
@@ -34,3 +34,12 @@ document.addEventListener("DOMContentLoaded", function() {
     settingContainer.appendChild(ul);
 
 });
+
+document.getElementById('editProfile').addEventListener('click', function () {
+    const overlay = document.getElementById('editProfile');
+    overlay.classList.add('active');
+
+    setTimeout(function () {
+        window.location.href = 'editprofile.html'; // Replace with your actual new page URL
+    }, 500); // Duration should match the CSS transition duration
+});
