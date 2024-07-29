d32e739e3ade73e3ecf0342d8be363cbabf4a954
mferns0978 <mfernandes0978@conestogac.on.ca>
2024-07-28 22:58:06 -0400

    michelle
    code

---
+++ js/editProfile.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.save-button').addEventListener('click', () => {
+        alert('Profile saved!');
+    });
+    
+    document.querySelector('.back-button').addEventListener('click', () => {
+        alert('Go back!');
+    });
+});
